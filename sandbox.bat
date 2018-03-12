30ca136c0b5775bf9aea53955dd7caa701707493
edward <reddtoric@users.noreply.github.com>
2018-03-12 21:55:48 +0000

    Update sandbox.bat

--- sandbox.bat
+++ sandbox.bat
@@ -11,6 +11,25 @@ set x[1][0]=a0
 set x[1][1]=a1
 set x[1][2]=a2
 
+setlocal EnableDelayedExpansion
+set /A i=0
+set /A k=0
+set /A j=1
+:loop2
+	if %i%==2 ( if %k%==0 ( goto :afterloop2 ))
+	::echo x[!i!][!k!] = !x[%i%][%k%]!
+	::call set !%x[%%i][%%k]%!=%j%
+	set x[!i!][!k!]=%j%
+	echo x[!i!][!k!] = !x[%i%][%k%]!
+	set /A j=j+1
+	set /A k=k+1
+	if %k%==3 (
+		set /A k=0
+		set /A i=i+1
+		goto :loop2
+	) else ( goto :loop2 )
+
+:afterloop2
 
 echo %x[0][0]%
 echo %x[0][1]%
